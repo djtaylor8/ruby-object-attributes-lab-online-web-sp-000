8223b913ed4effaea5dbec916d193b53ecc445cc
DJ Taylor <dtaylor88@gmail.com>
2021-06-02 22:52:53 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  
+  def name=(person_name)
+    @this_name = person_name
+  end 
+  
+  def name 
+    @this_name
+  end
+  
+  def job=(person_job)
+    @this_job = person_job
+  end
+  
+  def job
+    @this_job
+  end
+  
+end
